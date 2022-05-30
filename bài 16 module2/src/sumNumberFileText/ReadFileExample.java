0f0bcc6b898899f01d36e7763ca78eec38adc339
unknown <hairealmta@gmail.com>
2022-05-30 16:12:23 +0700

    ReadFile

---
+++ bài 16 module2/src/sumNumberFileText/ReadFileExample.java
@@ -0,0 +1,31 @@
+package sumNumberFileText;
+
+import java.io.*;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println("Tổng =" + sum);
+
+        } catch (Exception e) {
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+    }
+
+}
